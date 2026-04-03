7f8f659861804b83bdd1e6a1be20fc315a7cfc2a
achmeddavids98-collab <achmeddavids98@gmail.com>
2026-04-03 16:38:05 +0000

    Add main_pulse.py script

---
+++ main_pulse.py
@@ -0,0 +1,9 @@
+import BROCA_AREA
+import FRONTAL_LOBE
+import HIPPOCAMPUS
+import OCCIPITAL_LOBE
+import PARIETAL_LOBE
+import TEMPORAL_LOBE
+import THALAMUS
+
+print('Sovereign Node 2026: All 7 Regions Online')
